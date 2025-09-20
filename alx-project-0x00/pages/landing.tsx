90cafa88e4feb57d06539726062a14bc7af85404
yonas-baysasaw <yonas.baysasaw.kibret@gmail.com>
2025-09-20 23:48:20 +0300

    Using components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -4,7 +4,9 @@ const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
-      <Card></Card>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
