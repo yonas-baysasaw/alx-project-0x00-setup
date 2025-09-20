b5c250c896c2c48784e7ecf00f3ecf604fe95777
yonas-baysasaw <yonas.baysasaw.kibret@gmail.com>
2025-09-20 23:46:45 +0300

    Using components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,7 +1,11 @@
-const Pill: React.FC = () => {
+interface PillProps {
+  title: string;
+}
+
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+      <p className=" text-sm ">{title}</p>
     </div>
   );
 };
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,10 @@
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card></Card>
     </div>
   );
 };
